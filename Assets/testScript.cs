ee4a051d62dca55b880695bd7fe1b0c59c60275a
natedonato <natedonato@gmail.com>
2019-10-14 11:10:35 -0700

    first scripts

---
+++ Assets/testScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    int frameCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        print("Hey");
+        float distance = GetDistance(0, 5, 10, 15);
+        print(distance);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(frameCount);
+        frameCount = frameCount + 1;
+    }
+
+    float GetDistance(float x1, float y1, float x2, float y2)
+    {
+        float dX = x2 - x1;
+        float dy = y2 - y1;
+        float cSquared = (dX * dX) + (dy * dy);
+        float distance = Mathf.Sqrt(cSquared);
+        return distance;
+    }
+}
---
+++ Assets/timeGame.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeGame : MonoBehaviour
+{
+    float roundStartTime;
+    int waitTime;
+    float startDelayTime = 3;
+    bool inProgress = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Press Spacebar once you think the time has passed.");
+        Invoke("SetRandomTime", startDelayTime);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleInput();
+
+
+    }
+
+    void handleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && inProgress)
+        {
+            inProgress = false;
+            float playerWaitTime = Time.time - roundStartTime;
+            float error = Mathf.Abs(waitTime - playerWaitTime);
+
+            print("You waited for " + playerWaitTime + "seconds.");
+            print("Your difference was " + error + " seconds.");
+            Invoke("SetRandomTime", startDelayTime);
+        }
+    }
+
+    void SetRandomTime()
+    {
+        waitTime = Random.Range(3, 16);
+        roundStartTime = Time.time;
+        inProgress = true;
+        print(waitTime + " seconds.");
+    }
+
+}
